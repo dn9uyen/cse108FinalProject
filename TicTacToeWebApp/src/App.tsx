67fe24dcf54e473a879037a3461ddc531f2ffd93
dn9uyen <dannynguyen84268@gmail.com>
2024-04-25 00:27:30 -0700

    clean up

--- TicTacToeWebApp/src/App.tsx
+++ TicTacToeWebApp/src/App.tsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { io } from 'socket.io-client'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function sendSocket() {
+        let socket = io();
+        socket.on('connectEvent', () => {
+            socket.emit('testData', { data: 'I\'m connected!' });
+        });
+        console.log("Connected")
+        return "1"
+    }
+
+    return (
+        <>
+            <button onClick={ ()=>sendSocket() }>Send data</button>
+        </>
+    )
 }
 
 export default App
---
+++ backend/main.py
@@ -0,0 +1,14 @@
+import flask
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('connectEvent')
+def handle_message(data):
+    print('received message: ' + data)
+
+if __name__ == '__main__':
+    socketio.run(app)
