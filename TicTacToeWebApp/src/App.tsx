da18c81983ea3346ccb2e6e4ce3da56ef60b86ac
ianpagador <ian.pagador2@gmail.com>
2024-05-06 03:50:57 -0700

    Added test game board

--- TicTacToeWebApp/src/App.tsx
+++ TicTacToeWebApp/src/App.tsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
-import { io } from 'socket.io-client'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/login/login.tsx";
+import Register from "./pages/login/register.tsx";
+import Page from "./pages/testpage/page.tsx";
+import Game from './pages/testpage/Game.tsx';
 
 function App() {
 
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="page" element={<Page />} />
+                    <Route path="game" element={<Game />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
+/*
     function sendSocket() {
         var socket = io("http://127.0.0.1:5000/lobby");
         socket.on('connect', function() {
             socket.emit('my event', { data: 'I\'m connected!' });
         });
     }
-
-    return (
-        <>
+    <>
             <button onClick={() => sendSocket()}>Send data</button>
-        </>
-    )
-}
-
-export default App
+        </> */
---
+++ TicTacToeWebApp/src/pages/login/register.tsx
@@ -0,0 +1,95 @@
+import { Box, Button, Divider, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, Paper } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export default function Register() {
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [usernameError, setUsernameError] = React.useState(false);
+
+    const handleClickTogglePassword = () => { setShowPassword(!showPassword); }
+
+    // Submit account info to backend
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const username = form.username.value;
+        const password = form.password.value;
+
+        const response = await fetch("http://127.0.0.1:5000/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ "username": username, "password": password})
+        });
+        if (response.status != 403) {
+            setUsernameError(false);
+            navigate("/page");
+        }
+        else {
+            setUsernameError(true);
+        }
+    }
+
+    return (
+        <Box sx={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <Paper elevation={10} sx={{ padding: "15px", width: "max-content" }}>
+                <h3 style={{ textAlign: "center", margin: 0 }}>Account Creation</h3>
+
+                <form onSubmit={handleSubmit}>
+                    {/* Username */}
+                    <Paper elevation={5} sx={{ width: "max-content", marginTop: "15px" }}>
+                        <FormControl sx={{ width: "25ch" }} variant="outlined">
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <OutlinedInput
+                                required
+                                error={usernameError}
+                                name="username"
+                                type="text"
+                                label="Password"
+                            />
+                        </FormControl>
+                    </Paper >
+                    <FormHelperText error sx={{ visibility: usernameError ? "visible" : "hidden" }}>
+                        {usernameError ? "Username already exists" : ""}
+                    </FormHelperText>
+
+                    {/* Password */}
+                    <Paper elevation={5} sx={{ width: "max-content", marginTop: "20px" }}>
+                        <FormControl sx={{ width: "25ch" }} variant="outlined">
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <OutlinedInput
+                                required
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={handleClickTogglePassword}>
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                    </Paper >
+
+                    {/* Submit button */}
+                    <Box textAlign="center" sx={{ marginTop: "20px" }}>
+                        <Button type="submit" variant="contained" sx={{ padding: "10px", width: "100%" }} > Create Account</Button>
+                    </Box>
+                </form>
+
+                <Divider sx={{ marginTop: "10px", opacity: 1 }}>or</Divider>
+
+                <Box textAlign="center" sx={{ marginTop: "10px" }}>
+                    <Button onClick={() => { navigate("/") }} variant="outlined" sx={{ padding: "10px", width: "100%" }}>Login instead</Button>
+                </Box>
+            </Paper >
+        </Box>
+    )
+}
