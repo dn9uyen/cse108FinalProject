59b64489ce2e7f033a51c7f22bfbdcc1e6cb5a3a
Danny Nguyen <dannynguyen84268@gmail.com>
2024-05-07 01:29:59 -0700

    Fix lobby to game transition

--- TicTacToeWebApp/src/pages/Game.tsx
+++ TicTacToeWebApp/src/pages/Game.tsx
@@ -7,11 +7,13 @@ import Cookies from "js-cookie"
 export default function Game(props: any) { // TODO: pass game id from lobby
     const [chat, setChat] = useState<String[]>([""]);
     const [board, setBoard] = useState(Array(9).fill(-1));
-    const gameId = "123";
+    const urlParams = new URLSearchParams(window.location.search);
+    const gameId = urlParams.get('gameId');
 
     useEffect(() => {
         function onConnect() {
             gameSocket.emit('joinGame', { gameId: gameId, username: Cookies.get("username") });
+            console.log(gameSocket.connected)
         }
 
         function onDisconnect() {
@@ -20,6 +22,7 @@ export default function Game(props: any) { // TODO: pass game id from lobby
 
         function onChatBroadcast(json: any) {
             setChat(chat.concat([json["message"]]));
+            console.log("here")
         }
 
         function onGameStateUpdate(json: any) {
@@ -31,6 +34,9 @@ export default function Game(props: any) { // TODO: pass game id from lobby
         gameSocket.on('disconnect', onDisconnect);
         gameSocket.on('chatBroadcast', (json) => { onChatBroadcast(json) });
         gameSocket.on('gameStateUpdate', (json) => { onGameStateUpdate(json) });
+
+        gameSocket.connect();
+        console.log(gameSocket.listeners("chatBroadcast"))
 
         return () => {
             gameSocket.off('connect', onConnect);
@@ -50,22 +56,22 @@ export default function Game(props: any) { // TODO: pass game id from lobby
 
     return (
         <Box sx={{ display: 'flex', height: '100vh' }}>
-        <Box sx={{ width: '25%', backgroundColor: '#f0f0f0', padding: '20px' }}>
-            <ChatBox chat={chat} />
-        </Box>
-        <Box sx={{ flexGrow: 1, padding: '20px' }}>
-            <Box sx={{ backgroundColor: '#44344F', padding: '10px 0', textAlign: 'center', marginBottom: '20px' }}>
-                <Typography variant="h3" sx={{ color: '#fff' }}>TIC TAC TOE</Typography>
+            <Box sx={{ width: '25%', backgroundColor: '#f0f0f0', padding: '20px' }}>
+                <ChatBox chat={chat} gameId={gameId} />
             </Box>
+            <Box sx={{ flexGrow: 1, padding: '20px' }}>
+                <Box sx={{ backgroundColor: '#44344F', padding: '10px 0', textAlign: 'center', marginBottom: '20px' }}>
+                    <Typography variant="h3" sx={{ color: '#fff' }}>TIC TAC TOE</Typography>
+                </Box>
 
-            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '175px' }}>
-                <div className="board" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 100px)', gap: '5px', justifyContent: 'center' }}>
-                    {board.map((cell, index) => (
-                        <button key={index} onClick={() => handleClick(index)} style={{ width: '100px', height: '100px', fontSize: '2rem' }}>{cell}</button>
-                    ))}
-                </div>
+                <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '175px' }}>
+                    <div className="board" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 100px)', gap: '5px', justifyContent: 'center' }}>
+                        {board.map((cell, index) => (
+                            <button key={index} onClick={() => handleClick(index)} style={{ width: '100px', height: '100px', fontSize: '2rem' }}>{cell}</button>
+                        ))}
+                    </div>
+                </Box>
             </Box>
         </Box>
-    </Box>
     )
 }
